2093fba48776808e74374da7e400c731801c72ab
Jason Shen <jason.shen@pwc.com>
2022-07-13 22:29:19 +0800

    add

---
+++ react/build/precache-manifest.3dcdcfb1ce6d5866760aeb9bad852956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d68bd5e9043d1aeec951876218f4e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9382772e89f0a248eb7b",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0be7aad1ca9b8854eee4",
+    "url": "/static/js/2.43f2ee7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43f2ee7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9382772e89f0a248eb7b",
+    "url": "/static/js/main.2c72b389.chunk.js"
+  },
+  {
+    "revision": "783a1f65bab7b19c09bf",
+    "url": "/static/js/runtime-main.d67d11c1.js"
+  }
+]);
---
+++ react/build/static/js/main.2c72b389.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphello=this.webpackJsonphello||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var l=t(0),o=t.n(l),r=t(2),c=t.n(r);t(8);var u=function(){return o.a.createElement("div",null,"Hello ","World")};c.a.render(o.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.2c72b389.chunk.js.map
